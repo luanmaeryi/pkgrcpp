f0f12170436717837168ec2355b7ac533a69dbb5
yume <ab02200059@163.com>
2024-06-27 22:42:06 +0800

    add documentation

--- src/add_cpp.cpp
+++ src/add_cpp.cpp
@@ -1,8 +1,15 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
+//' Add Two Numbers
+//'
+//' Return the sum of two numbers.
+//' @param x
+//' @param y
+//' @export
 // [[Rcpp::export]]
 double add_cpp(double x, double y) {
   double value = x + y;
   return value;
 }
+
